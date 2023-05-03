88f3f0d1f5981d4cb7d4b099e26ba89ea21f4418
Danils-sd <116029461+Danils-sd@users.noreply.github.com>
2023-05-03 19:52:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,16 @@
 #include "Adres.h"
 #include "FIO.h"
 
-using namespace std;
+
 
 int _otvet1;
+int _otvet2;
+int _otvet3;
 
 vector<User> date;
 int unikKod = 0;
 //Функции вывода меню
-void manu(){
+void Manu(){
     cout << "------Выберите действие------" << endl
     << "(0) Вернуться в меню" << endl
     << "(1) Вывести всю базу данных" << endl
@@ -20,26 +22,248 @@ void manu(){
     << "(4) Удалить данные" << endl
     << "Ответ: ";
     cin >> _otvet1;
+
 }
 
-void manuPoisk(){
+void ManuPoisk(){
     cout << "------Варианты поиска------" << endl
          << "(0) По коду" << endl
-         << "(1) По ФИО" << endl
+         << "(1) По фамилии" << endl
          << "(2) По номеру телефона" << endl
          << "(3) По номеру карты" << endl
          << "(4) Вернуться в меню" << endl
          << "Ответ: ";
+    cin >> _otvet2;
 }
 
-void dobav(){
+
+void ManuDell(){
+    cout << "------Варианты удаления------" << endl
+         << "(0) По коду" << endl
+         << "(1) По фамилии" << endl
+         << "(2) По номеру телефона" << endl
+         << "(3) По номеру карты" << endl
+         << "(4) Вернуться в меню" << endl
+         << "Ответ: ";
+    cin >> _otvet3;
+}
+
+void VivodUser(int i){
+    int kod = date.at(i).getKod();
+    string lName = date.at(i).getFio().getLastName();
+    string name = date.at(i).getFio().getName();
+    string sName = date.at(i).getFio().getSurname();
+    string kard = date.at(i).getKard();
+    string number = date.at(i).getNumber();
+    string country = date.at(i).getAdres().getCountry();
+    string city = date.at(i).getAdres().getCity();
+    string streat = date.at(i).getAdres().getStreat();
+    string house = date.at(i).getAdres().getHouse();
+    string floor = date.at(i).getAdres().getFloor();
+    string appartament = date.at(i).getAdres().getApartament();
+    cout << "----------------------------------" << endl;
+    cout << "| " << kod << " | " << lName << " " << name << " "  << sName << endl;
+    cout << "----------------------------------" << endl;
+    cout << "| " << kard << endl;
+    cout << "----------------------------------" << endl;
+    cout << "| " << number << endl;
+    cout << "----------------------------------" << endl;
+    cout << "| " << "Страна: " << country << endl;
+    cout << "| " << "Город: " << city << endl;
+    cout << "| " << "Улица: " << streat << endl;
+    cout << "| " << "Дом: " << house << endl;
+    cout << "| " << "Этаж: " << floor << endl;
+    cout << "| " << "Квартира: " << appartament << endl;
+    cout << "----------------------------------" << endl;
+}
+
+void Poisk(vector<User>& d, int a){
+
+    string otv;
+    switch (a) {
+        case 0:
+
+            cout << "Введите код: ";
+            cin >> otv;
+            for(int i = 0; i < d.size(); i++){
+                if(otv == to_string(d.at(i).getKod())){
+                    VivodUser(i);
+                }
+            }
+
+            Manu();
+            break;
+        case 1:
+
+            cout << "Введите фамилию: ";
+            cin >> otv;
+
+            for(int i = 0; i < d.size(); i++){
+                if(otv == d.at(i).getFio().getLastName()){
+                    VivodUser(i);
+                }
+            }
+
+            Manu();
+            break;
+        case 2:
+
+            cout << "Введите номер телефона: ";
+            cin >> otv;
+
+            for(int i = 0; i < d.size(); i++){
+                if(otv == d.at(i).getNumber()){
+                    VivodUser(i);
+                }
+            }
+
+            Manu();
+            break;
+        case 3:
+
+            cout << "Введите номер карты: ";
+            cin >> otv;
+
+            for(int i = 0; i < d.size(); i++){
+                if(otv == d.at(i).getKard()){
+                    VivodUser(i);
+                }
+            }
+
+            Manu();
+            break;
+        case 4:
+
+
+            Manu();
+            break;
+        default:
+            Manu();
+
+    }
+}
+
+void Dell(vector<User>& d, int a){
+    string otv1;
+    switch (a) {
+        case 0:
+            cout << "Введите код: ";
+            cin >> otv1;
+            for(int i = 0; i < d.size(); i++){
+                if(otv1 == to_string(d.at(i).getKod())){
+                    VivodUser(i);
+                    int otvV;
+                    cout << "Удалить этого пользователя?" << endl;
+                    cout << "(0) - Да" << endl;
+                    cout << "(1) - Нет" << endl;
+                    cout << "Ответ: ";
+                    cin >> otvV;
+
+                    if(otvV == 0){
+                        d.erase(d.cbegin() + i);
+                    }
+                    else {
+                        break;
+                    }
+
+                }
+            }
+            Manu();
+            break;
+
+
+        case 1:
+            cout << "Введите фамилию: ";
+            cin >> otv1;
+
+            for(int i = 0; i < d.size(); i++){
+                if(otv1 == d.at(i).getFio().getLastName()){
+                    VivodUser(i);
+                    int otvV;
+                    cout << "Удалить этого пользователя?" << endl;
+                    cout << "(0) - Да" << endl;
+                    cout << "(1) - Нет" << endl;
+                    cout << "Ответ: ";
+                    cin >> otvV;
+
+                    if(otvV == 0){
+                        d.erase(d.cbegin() + i);
+                    }
+                    else {
+                        break;
+                    }
+
+                }
+            }
+            Manu();
+            break;
+        case 2:
+            cout << "Введите номер телефона: ";
+            cin >> otv1;
+
+            for(int i = 0; i < d.size(); i++){
+                if(otv1 == d.at(i).getNumber()){
+                    VivodUser(i);
+                    int otvV;
+                    cout << "Удалить этого пользователя?" << endl;
+                    cout << "(0) - Да" << endl;
+                    cout << "(1) - Нет" << endl;
+                    cout << "Ответ: ";
+                    cin >> otvV;
+
+                    if(otvV == 0){
+                        d.erase(d.cbegin() + i);
+                    }
+                    else {
+                        break;
+                    }
+
+                }
+            }
+
+            Manu();
+            break;
+        case 3:
+            cout << "Введите номер карты: ";
+            cin >> otv1;
+
+            for(int i = 0; i < d.size(); i++){
+                if(otv1 == d.at(i).getKard()){
+                    VivodUser(i);
+                    int otvV;
+                    cout << "Удалить этого пользователя?" << endl;
+                    cout << "(0) - Да" << endl;
+                    cout << "(1) - Нет" << endl;
+                    cout << "Ответ: ";
+                    cin >> otvV;
+
+                    if(otvV == 0){
+                        d.erase(d.cbegin() + i);
+                    }
+                    else {
+                        break;
+                    }
+
+                }
+            }
+            Manu();
+            break;
+        case 4:
+            Manu();
+            break;
+        default:
+            Manu();
+    }
+}
+
+void Dobav(){
     string name, lastName, surname;
 
     string country, city, streat, house, floor, appartament;
 
     string number;
 
-    int kard;
+    string kard;
 
     cout << "------Заполните данные------" << endl;
     cout << "Введите имя: ";
@@ -93,6 +317,7 @@ void dobav(){
 
     User user;
     user.base(unikKod+1, fio, number, adr, kard);
+    unikKod += 1;
     cout << "Данные записаны" << endl;
     user.update(date, user);
 
@@ -100,57 +325,47 @@ void dobav(){
 
 
 int main() {
-    manu();
-    while (_otvet1 != 5){
+    Manu();
+
+    while(_otvet1 != 5) {
+        switch (_otvet1) {
+            case 0:
 
 
-        if(_otvet1 == 0){//Вернуться в Главное меню
-            manu();
-        }
-        else if(_otvet1 == 1){//Вывод всей БД
+                Manu();
+                break;
+            case 1:
+                cout << "---------База данных---------" << endl;
+                for (int i = 0; i < date.size(); i++) {
+                    VivodUser(i);
+                }
+                Manu();
+                break;
+            case 2:
+                ManuPoisk();
 
-            for(int i = 0; i < date.size(); i++){
-                string a = date.at(i).getFio().getName();
-                cout << a;
-            }
-            manu();
+                Poisk(date, _otvet2);
+                Manu();
+                break;
+            case 3:
+                Dobav();
 
-        }
-        else if(_otvet1 == 2){//Поиск по БД
-            manuPoisk();
-            int otvet2;
-            cin >> otvet2;
-            if(otvet2 == 0){//Поиск по коду
 
-            }
-            else if(otvet2 == 1){//Поиск по ФИО
-                cout << "data" << endl;
-            }
-            else if(otvet2 == 2){//Поиск по номеру телефона
-                manuPoisk();
+                Manu();
+                break;
+            case 4:
+                ManuDell();
 
-            }
-            else if(otvet2 == 3){//Поиск по номеру карты
-                cout << "dobav" << endl;
-            }
-            else if(otvet2 == 4){//Вернуться в меню
-                cout << "del" << endl;
-            }
-            else{//Иное
-                cout << "err" << endl;
+                Dell(date, _otvet3);
+                Manu();
 
-            }
-        }
-        else if(_otvet1 == 3){//Добавление элемента
-            dobav();
-            manu();
-        }
-        else if(_otvet1 == 4){//Удаление элемента
-
-        }
-        else{//Иной ответ
-
+                break;
+            default:
+                Manu();
 
         }
     }
+
+    
+    return 0;
 }

aa8bb1263e7f5799fad213a0914518b29c9a928c
Danils-sd <116029461+Danils-sd@users.noreply.github.com>
2023-05-03 19:49:58 +0300

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -1,9 +1,6 @@
-//
-// Created by Danila on 28.03.2023.
-//
 #include <vector>
 #include "User.h"
-void User::base(int a, FIO b, std::string c, Adres d, int e) {
+void User::base(int a, FIO b, std::string c, Adres d, std::string e) {
     kod = a;
     fio = b;
     tNumber = c;
@@ -15,9 +12,7 @@ void User::update(vector<User>& d, User a) {
     d.push_back(a);
 }
 
-void User::del(vector<User>& d, int a) {
-    d.pop_back();
-}
+
 
 int User::getKod() {
     return kod;
@@ -35,6 +30,6 @@ Adres User::getAdres() {
     return adres;
 }
 
-int User::getKard() {
+string User::getKard() {
     return kard;
 }
